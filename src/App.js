a9685f03de9a7b2f8fbc68ccce03c14b95f3c3ae
Benjamin Small <smallbenjamin94@gmail.com>
2022-05-16 16:18:19 -0700

    built initial form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
+import Form from 'react-bootstrap/Form';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+
+    return (
+      <>
+        <h1>City Explorer</h1>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Label htmlFor="city-name">City Name</Form.Label>
+          <Form.Control type="text" id="city-name" onInput="this.handleCityInput"></Form.Control>
+        </Form>
+      </>
+    );
+  }
 }
 
 export default App;
